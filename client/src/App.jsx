f6fd57f1f74b164b5f967c77f4963256fe778563
Arun <arunmundakkal003@gmail.com>
2025-03-13 15:39:19 +0530

    Refactor routing to use BrowserRouter; add Vercel configuration for rewrites

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
 import { useState, useEffect } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LoginPage } from "./Login/Login";
 import { Student } from "./Users/Student/Student";
 import { CertificateUploadPage } from "./Users/Upload/Upload";
-import {NotificationContainer } from "./Notification/NotificationContainer";
-import "./App.css"
+import { NotificationContainer } from "./Notification/NotificationContainer";
+import "./App.css";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [token, setToken] = useState(null);
 
   // Check localStorage for a stored token on initial load
-
   useEffect(() => {
     const storedToken = localStorage.getItem("token");
     if (storedToken) {
@@ -36,7 +35,7 @@ function App() {
 
   return (
     <Router>
-      <NotificationContainer/>
+      <NotificationContainer />
       <Routes>
         <Route 
           path="/" 
@@ -46,7 +45,7 @@ function App() {
           path="/dashboard" 
           element={isLoggedIn ? <Student token={token} onLogout={handleLogout} /> : <LoginPage onLoginSuccess={handleLoginSuccess} />}
         />
-         <Route path="/upload-certificate" element={<CertificateUploadPage />} />
+        <Route path="/upload-certificate" element={<CertificateUploadPage />} />
       </Routes>
     </Router>
   );
