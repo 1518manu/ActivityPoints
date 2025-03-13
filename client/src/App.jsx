1be7c33497ab0a433d7bf764955e342fdd4b3554
Arun <arunmundakkal003@gmail.com>
2025-03-13 15:33:22 +0530

    Change router from BrowserRouter to HashRouter for improved routing compatibility

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { LoginPage } from "./Login/Login";
 import { Student } from "./Users/Student/Student";
 import { CertificateUploadPage } from "./Users/Upload/Upload";
